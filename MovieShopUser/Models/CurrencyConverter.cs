995a01e2bba262d8633e1181c4259b8b364fc9d6
Benc0054 <benc0054@easv365.dk>
2016-11-16 21:28:54 +0100

    CurrencyConverter

---
+++ MovieShopUser/Models/CurrencyConverter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MovieShopUser.Models
+{
+
+    public enum Currency { DKK, USD, EUR };
+
+    public class CurrencyConverter
+    {
+        
+        public readonly double USD = 0.143797;
+        public readonly double EUR = 0.134377161;
+
+        public static Currency currency;
+
+        public void Setcurrency(Currency money)
+        {
+            currency = money;
+        }
+
+        public double Convert(double price)
+        {
+            if (currency == Currency.USD)
+            {
+                double USDprice = price / USD;
+                return USDprice;
+            }
+
+            if (currency == Currency.EUR)
+            {
+                double EURprice = price / EUR;
+                return EURprice;
+            }
+            else
+                return price;
+        }
+    }    
+}
